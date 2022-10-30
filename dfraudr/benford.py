cf881ea1d2da52193616973095bb61e24199fe9b
Matt Nash <mattnashbrowns@gmail.com>
2022-10-30 14:22:36 -0400

    some basic functions added

--- dfraudr/benford.py
+++ dfraudr/benford.py
@@ -1,35 +1,34 @@
-#!/usr/bin/env python3
- 
 import pandas as pd
 import numpy as np
 
-class Benford:
-    def __init__(self,base=10):
-        self.base = base
-        self.probabilities = _get_probs()
 
-    # Takes a list of numbers as input, returns a list of the first digits
-    def first_digits(numbers):
-        firsts = [ str(x)[0] for x in numbers ]
-        return firsts
+# Takes a list of numbers as input, returns a list of the first digits
+def first_digits(numbers):
+    firsts = [ int(str(x)[0]) for x in numbers ]
+    return firsts
+
+# get a list of Benford probabilities for the non-zero digits of an N-based number system
+# N defaults to 10
+def get_probs(base=10):
+    numbers = range(1, base)
+    probabilities = [ np.log10(1 + (1/x)) for x in numbers ]
+    return probabilities
     
-    # get a list of Benford probabilities for the non-zero digits of an N-based number system
-    # N defaults to 10
-    def _get_probs(self):
-        numbers = range(1, self.base)
-        probabilities = [ np.log10(1 + (1/x)) for x in numbers ]
-        return probabilities
+# Takes a list of numbers presumed to be naturally-occurring
+# Returns a list of counts for each digit from 1 to (base - 1)
+# containing the frequency of each one's occurrence in the list
+def get_freqs(numbers,base=10):
+    all_digits = range(1,base)
+    digits = first_digits(numbers)
+    unique, counts = np.unique(digits, return_counts=True)
+    counts_dict = dict(zip(unique,counts))
+    freqs = []
+    for x in all_digits:
+        if x not in counts_dict:
+            counts_dict[x] = 0
+    count_df = pd.DataFrame(counts_dict)
+    return counts_dict
         
-    # Takes a list of numbers presumed to be naturally-occurring
-    # Returns a list of counts for each digit from 1 to (base - 1)
-    # containing the frequency of each one's occurrence in the list
-    def get_freqs(numbers):
-        all_digits = range(1,self.base)
-        digits = first_digits(numbers)
-        unique, counts = np.unique(digits, return_counts=True)
-        missing = [ x for x in all_digits if x not in unique ]
-        for x in missing:
-            counts.insert(x - 1, 0)
-        return counts
+    
 
         
