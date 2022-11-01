2c49d550ba959efea22638357e820466e603008f
Matt Nash <mattnashbrowns@gmail.com>
2022-11-01 15:35:57 -0400

    config troubles

---
+++ dfraudr/config.py
@@ -0,0 +1,14 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+SECRET_KEY = environ.get('SECRET_KEY')
+
+DATABASE='dfraudr/dfraudr.sqlite'
+
+UPLOAD_LOCATION = 'instance/uploads'
+
+    
